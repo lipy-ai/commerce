436ebdf158caf80ffbd6dd2e2d22fb922b4c9772
Kundan Bhosale <kundanmbhosale@gmail.com>
2025-05-18 13:29:49 +0530

    update global.ts

--- apps/server/src/middlewares/global.ts
+++ apps/server/src/middlewares/global.ts
@@ -12,7 +12,7 @@ export const globalMiddleware: MiddlewareHandler<ServerContext> = async (
   const userAgent = c.req.header("User-Agent");
   const isBot = isbot(userAgent);
   const validKey = c.req.header("M2M-Key") === env.VITE_M2M_KEY.toString();
-  if ((isBot && validKey) || !validKey) {
+  if ((isBot && !validKey) || !validKey) {
     logger.warn("Blocked an due to missing headers / bot activity...");
     throw new HTTPException(401, {
       message: "Unauthorized!",

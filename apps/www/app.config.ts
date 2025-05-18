0ab5d817ad0ce1a6d363c3a6ff502c435a944b89
Kundan Bhosale <kundanmbhosale@gmail.com>
2025-05-18 14:58:11 +0530

    update 7 files

--- apps/www/app.config.ts
+++ apps/www/app.config.ts
@@ -21,9 +21,6 @@ export default defineConfig({
             "Content-Type": "application/json",
           },
         },
-        headers: {
-          "Content-Type": "application/json",
-        },
       },
     },
   },

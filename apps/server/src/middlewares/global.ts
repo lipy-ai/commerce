937d36eaa85afc4d8f4458996f492188d8f4868f
Kundan Bhosale <kundanmbhosale@gmail.com>
2025-05-18 13:31:00 +0530

    update package.json, global.ts and bun.lockb

--- apps/server/src/middlewares/global.ts
+++ apps/server/src/middlewares/global.ts
@@ -3,17 +3,14 @@ import { logger } from "@/lib/logger";
 import { ServerContext } from "@/types";
 import { type MiddlewareHandler } from "hono";
 import { HTTPException } from "hono/http-exception";
-import { isbot } from "isbot";
 
 export const globalMiddleware: MiddlewareHandler<ServerContext> = async (
   c,
   next
 ) => {
-  const userAgent = c.req.header("User-Agent");
-  const isBot = isbot(userAgent);
   const validKey = c.req.header("M2M-Key") === env.VITE_M2M_KEY.toString();
-  if ((isBot && !validKey) || !validKey) {
-    logger.warn("Blocked an due to missing headers / bot activity...");
+  if (!validKey) {
+    logger.warn("Blocked an due to missing M2M key...");
     throw new HTTPException(401, {
       message: "Unauthorized!",
     });

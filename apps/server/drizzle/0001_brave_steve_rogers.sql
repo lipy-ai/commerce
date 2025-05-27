698faf10fd7f22a05bff17fc7001160f4b87c2e7
Kundan Bhosale <kundanmbhosale@gmail.com>
2025-05-27 15:13:55 +0530

    synced

--- apps/server/drizzle/0001_brave_steve_rogers.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "lipy"."cart" DROP CONSTRAINT "cart_organization_id_org_id_fk";
---> statement-breakpoint
-ALTER TABLE "lipy"."cart" DROP CONSTRAINT "cart_user_id_organization_id_pk";--> statement-breakpoint
-ALTER TABLE "lipy"."cart" ADD CONSTRAINT "cart_user_id_pk" PRIMARY KEY("user_id");--> statement-breakpoint
-ALTER TABLE "lipy"."cart" DROP COLUMN "organization_id";
--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,48 +1,48 @@
-import { Kysely, PostgresDialect, sql } from "kysely";
-import { Pool } from "pg";
-import { logger } from "../lib/logger";
-import env from "../env";
-import { dbTables } from "./schema";
 import type { Kyselify } from "drizzle-orm/kysely";
 import type { PgTableWithColumns } from "drizzle-orm/pg-core";
+import { Kysely, PostgresDialect, sql } from "kysely";
+import { Pool } from "pg";
+import env from "../env";
+import { logger } from "../lib/logger";
+import { dbTables } from "./schema";
 
 export const pool = new Pool({ connectionString: env.DATABASE_URL });
 
 const dbDialect = new PostgresDialect({
-  pool,
+	pool,
 });
 
-const dbClient = new Kysely<Database>({
-  dialect: dbDialect,
+export const dbClient = new Kysely<Database>({
+	dialect: dbDialect,
 });
 
 export async function pingDatabase() {
-  try {
-    await sql`SELECT 1`.execute(db);
-    logger.info("✅ Database is reachable!");
-  } catch (err) {
-    logger.error("❌ Failed to connect to database:", err);
-  }
+	try {
+		await sql`SELECT 1`.execute(db);
+		logger.info("✅ Database is reachable!");
+	} catch (err) {
+		logger.error("❌ Failed to connect to database:", err);
+	}
 }
 
 export const db = dbClient.withSchema("lipy");
 
 function createTableMap<T extends Record<string, PgTableWithColumns<any>>>(
-  schemas: T
+	schemas: T,
 ): { [K in keyof T as `${T[K]["_"]["name"] & string}`]: T[K] } {
-  const result = {} as {
-    [K in keyof T as `${T[K]["_"]["name"] & string}`]: T[K];
-  };
+	const result = {} as {
+		[K in keyof T as `${T[K]["_"]["name"] & string}`]: T[K];
+	};
 
-  for (const key in schemas) {
-    const table = schemas[key];
-    if (!table || !table._) break;
-    const k = `${table._.name}` as `${(typeof table)["_"]["name"] & string}`;
-    console.log(key, k, table._.name);
-    result[k] && table && Object.assign(result[k], table);
-  }
+	for (const key in schemas) {
+		const table = schemas[key];
+		if (!table || !table._) break;
+		const k = `${table._.name}` as `${(typeof table)["_"]["name"] & string}`;
+		console.log(key, k, table._.name);
+		result[k] && table && Object.assign(result[k], table);
+	}
 
-  return result;
+	return result;
 }
 
 const tables = createTableMap(dbTables);
@@ -50,9 +50,9 @@ const tables = createTableMap(dbTables);
 export type DBTableMap = typeof tables;
 
 export type DBTypes = {
-  [K in keyof DBTableMap]: DBTableMap[K]["$inferSelect"];
+	[K in keyof DBTableMap]: DBTableMap[K]["$inferSelect"];
 };
 
 export type Database = {
-  [K in keyof typeof tables]: Kyselify<(typeof tables)[K]>;
+	[K in keyof typeof tables]: Kyselify<(typeof tables)[K]>;
 };

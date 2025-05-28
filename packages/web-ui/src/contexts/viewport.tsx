d1454014b4b64a6765b93ce8496e0b027f54b349
Kundan Bhosale <kundanmbhosale@gmail.com>
2025-05-28 12:22:42 +0530

    updates

--- packages/web-ui/src/contexts/viewport.tsx
+++ packages/web-ui/src/contexts/viewport.tsx
@@ -4,16 +4,16 @@ import {
 	type ReactNode,
 	createContext,
 	useContext,
-	// useLayoutEffect,
-	// useState,
+	useLayoutEffect,
+	useState,
 } from "react";
-// import { useDebouncedCallback } from "use-debounce";
+import { useDebouncedCallback } from "use-debounce";
 
 const viewportContext = createContext({
-	// width: 0,
-	// height: 0,
-	// screenWidth: 0,
-	// screenHeight: 0,
+	width: 0,
+	height: 0,
+	screenWidth: 0,
+	screenHeight: 0,
 	isMobile: false,
 });
 
@@ -24,47 +24,45 @@ const ViewportProvider = ({
 	children: ReactNode;
 	isMobile?: boolean;
 }) => {
-	// const initial = isMobile ? 768 : 1250;
+	const [width, setWidth] = useState(0);
+	const [height, setHeight] = useState(0);
+	const [screenWidth, setScreenWidth] = useState(0);
+	const [screenHeight, setScreenHeight] = useState(0);
 
-	// const [width, setWidth] = useState(0);
-	// const [height, setHeight] = useState(0);
-	// const [screenWidth, setScreenWidth] = useState(0);
-	// const [screenHeight, setScreenHeight] = useState(0);
+	const handleWindowResize = useDebouncedCallback(
+		// function
+		() => {
+			if (typeof window === "undefined") return;
+			setWidth(window.innerWidth);
+			setHeight(window.innerHeight);
+			setScreenWidth(window.screen.width);
+			setScreenHeight(window.screen.height);
 
-	// const handleWindowResize = useDebouncedCallback(
-	// 	// function
-	// 	() => {
-	// 		if (typeof window === "undefined") return;
-	// 		setWidth(window.innerWidth);
-	// 		setHeight(window.innerHeight);
-	// 		setScreenWidth(window.screen.width);
-	// 		setScreenHeight(window.screen.height);
-
-	// 		document.body.style.opacity = "1";
-	// 	},
-	// 	// delay in ms
-	// 	500,
-	// );
-	// useLayoutEffect(() => {
-	// 	if (typeof window === "undefined") return;
-	// 	if (width === 0 && height === 0) {
-	// 		handleWindowResize();
-	// 	} else {
-	// 		window.addEventListener("resize", handleWindowResize);
-	// 		return () => {
-	// 			window.removeEventListener("resize", handleWindowResize);
-	// 		};
-	// 	}
-	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	// }, [width, height]);
+			document.body.style.opacity = "1";
+		},
+		// delay in ms
+		500,
+	);
+	useLayoutEffect(() => {
+		if (typeof window === "undefined") return;
+		if (width === 0 && height === 0) {
+			handleWindowResize();
+		} else {
+			window.addEventListener("resize", handleWindowResize);
+			return () => {
+				window.removeEventListener("resize", handleWindowResize);
+			};
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [width, height]);
 
 	return (
 		<viewportContext.Provider
 			value={{
-				// width: width || initial,
-				// height: height || initial,
-				// screenWidth: screenWidth || initial,
-				// screenHeight: screenHeight || initial,
+				width: width,
+				height: height,
+				screenWidth: screenWidth,
+				screenHeight: screenHeight,
 				isMobile: isMobile || false,
 			}}
 		>
